ed92c5081fe4164773f85d5a2b02cdb2775a51bd
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-18 17:18:32 +0700

    membuat dao dan fungsi mengambil data dokter dengan id

--- app/src/main/java/com/tugas/ucp2/data/dao/DokterDao.kt
+++ app/src/main/java/com/tugas/ucp2/data/dao/DokterDao.kt
@@ -1,15 +1,21 @@
 package com.tugas.ucp2.data.dao
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.tugas.ucp2.data.entity.Dokter
 
+
+@Dao
 interface DokterDao {
     @Insert
     suspend fun insertDokter(dokter: Dokter)
 
-    @Query("SELECT * FROM dokter ORDER BY nama ASC")
-    fun getAllDokter(): List<Dokter>
+    @Query("SELECT * FROM dokter")
+    fun getAllDokters(): LiveData<List<Dokter>>
 
-
+    // Mengambil data dokter berdasarkan ID
+    @Query("SELECT * FROM dokter WHERE id = :dokterId")
+    suspend fun getDokterById(dokterId: Int): Dokter?
 }
