aa45dae96f9904a21a068699c38f87e007709629
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-23 03:14:45 +0700

    memmbuat ulang database

--- app/src/main/java/com/tugas/ucp2/data/database/AppDatabase.kt
+++ app/src/main/java/com/tugas/ucp2/data/database/AppDatabase.kt
@@ -9,26 +9,22 @@ import com.tugas.ucp2.data.dao.JadwalDao
 import com.tugas.ucp2.data.entity.Dokter
 import com.tugas.ucp2.data.entity.Jadwal
 
-
-@Database(entities = [Dokter::class, Jadwal::class], version = 1, exportSchema = false)
+@Database(entities = [Dokter::class,Jadwal::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
-    // Deklarasi DAO untuk Dokter
     abstract fun dokterDao(): DokterDao
-
-    // Deklarasi DAO untuk Jadwal
     abstract fun jadwalDao(): JadwalDao
 
     companion object {
         @Volatile
         private var Instance: AppDatabase? = null
 
-        fun getDatabase(context: Context): AppDatabase{
-            return (Instance ?: synchronized(this){
+        fun getDatabase(context: Context): AppDatabase {
+            return (Instance ?: synchronized(this) {
                 Room.databaseBuilder(
                     context.applicationContext,
-                    AppDatabase::class.java, //class database
-                    "KrsDatabase" //nama database
+                    AppDatabase::class.java,
+                    "RumahSakit"
                 )
                     .build().also { Instance = it }
             })
