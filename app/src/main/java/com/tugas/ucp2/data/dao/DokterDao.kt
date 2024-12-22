8843f8095ca83fcbb37710c09b7a8f78cebbf9b2
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-23 03:17:22 +0700

    membuat ulang dokter dao

--- app/src/main/java/com/tugas/ucp2/data/dao/DokterDao.kt
+++ app/src/main/java/com/tugas/ucp2/data/dao/DokterDao.kt
@@ -12,10 +12,6 @@ interface DokterDao {
     @Insert
     suspend fun insertDokter(dokter: Dokter)
 
-    @Query("SELECT * FROM dokter")
-    fun getAllDokters(): Flow<List<Dokter>>  // Menggunakan Flow sebagai pengganti LiveData
-
-    // Mengambil data dokter berdasarkan ID
-    @Query("SELECT * FROM dokter WHERE id = :dokterId")
-    fun getDokterById(dokterId: Int): Flow<Dokter?>  // Menggunakan Flow sebagai pengganti LiveData
+    @Query("SELECT * FROM dokter ORDER BY nama ASC")
+    fun getAllDokter(): Flow<List<Dokter>>
 }
