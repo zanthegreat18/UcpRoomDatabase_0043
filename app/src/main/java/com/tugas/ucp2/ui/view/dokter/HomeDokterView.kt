d3519e1d29d6da6e7d61b3e78db90a908d7b0cf6
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-24 12:54:03 +0700

    membuat home dokter view untuk tampilan dan fungsi pemanggilan

---
+++ app/src/main/java/com/tugas/ucp2/ui/view/dokter/HomeDokterView.kt
@@ -0,0 +1,435 @@
+package com.tugas.ucp2.ui.view.dokter
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tugas.ucp2.R
+import com.tugas.ucp2.data.entity.Dokter
+import com.tugas.ucp2.ui.customwidget.TopAppBar
+import com.tugas.ucp2.ui.viewmodel.HomeDokterViewModel
+import com.tugas.ucp2.ui.viewmodel.HomeUiStateDokter
+import com.tugas.ucp2.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun HomeDokterView(
+    viewModel: HomeDokterViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddDokter: () -> Unit = { },
+    onSeeDetail: () -> Unit = { },
+    onSeeJadwal: () -> Unit = { },
+    onSeeDokter: () -> Unit = { },
+    modifier: Modifier = Modifier
+){
+
+
+    val MyCustomGreen = Color(0xFF9DBC98)
+    val MyCustomContentColor = Color(0xFF3E3232)
+
+    Scaffold(
+        topBar = {
+            Column {
+                Spacer(modifier = Modifier.height(20.dp))
+                HeaderOjan(
+                    namaPerusahaan = "Ayo Sehat",
+                    profilResId = R.drawable.iconlogo1
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround,
+
+                    ) {
+                    Text(
+                        text = "Selamat Datang Kawan\uD83D\uDC4B",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Image(
+                        painter = painterResource(R.drawable.medical_staff),
+                        contentDescription = "Profile",
+                        modifier = Modifier
+                            .size(150.dp)
+                    )
+                }
+                TopAppBar(
+                    judul = "List Dokter",
+                    showBackButton = false,
+                    onBack = { }
+                )
+            }
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MyCustomGreen,
+                contentColor = MyCustomContentColor,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    IconButton(onClick = { onSeeJadwal() }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Jadwal"
+                        )
+                    }
+                    IconButton(onClick = { onAddDokter() }) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "Add Doctor"
+                        )
+                    }
+                }
+            }
+        },
+        containerColor = MaterialTheme.colorScheme.surface
+    ) {
+        innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeDokterView(
+            homeUiStateDokter = homeUiState,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun HeaderOjan(
+    namaPerusahaan: String,
+    profilResId: Int
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(
+                RoundedCornerShape(
+                    bottomStart = 30.dp,
+                    bottomEnd = 30.dp
+                )
+            )
+            .background(Color(0xFF9DBC98))
+            .padding(30.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween // Menyebarkan konten ke kiri dan kanan
+        ) {
+            // Kolom untuk teks di sebelah kiri
+            Column {
+                Text(
+                    text = namaPerusahaan,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White
+                )
+
+                Spacer(modifier = Modifier.height(6.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Anda Sehat, Kami Sakit",
+                        fontSize = 16.sp,
+                        color = Color.White.copy(alpha = 0.9f)
+                    )
+                }
+            }
+
+            // Box untuk logo di sebelah kanan
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color.White.copy(alpha = 0.5f))
+                    .border(2.dp, Color.White, CircleShape)
+            ) {
+                Image(
+                    painter = painterResource(profilResId),
+                    contentDescription = "Perusahaan Logo",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .align(Alignment.Center),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun BodyHomeDokterView(
+    homeUiStateDokter: HomeUiStateDokter,
+    onClick: (Int) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(
+                brush = Brush.radialGradient(
+                    colors = listOf(
+                        MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.3f),
+                        MaterialTheme.colorScheme.background
+                    )
+                )
+            )
+    ){
+        when {
+            homeUiStateDokter.isLoading -> {
+                Column(
+                    modifier = Modifier.align(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CircularProgressIndicator(
+                        color = Color(0xFF9DBC98),
+                        strokeWidth = 4.dp
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Loading bang...",
+                        color = Color(0xFF9DBC98),
+                        fontSize = 14.sp,
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            }
+
+            homeUiStateDokter.isError -> {
+                Column(
+                    modifier = Modifier.align(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Warning,
+                        contentDescription = "Error icon",
+                        tint = Color(0xFF9DBC98),
+                        modifier = Modifier.size(40.dp)
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Terjadi Kesalahan!",
+                        color = MaterialTheme.colorScheme.error,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+            homeUiStateDokter.listDokter.isEmpty() -> {
+                Column(
+                    modifier = Modifier.align(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = "NO data icon",
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.size(40.dp)
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Tidak Ada Data Woi.",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            }
+
+            else -> {
+                LazyColumn(
+                    contentPadding = PaddingValues(vertical = 16.dp, horizontal = 12.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(homeUiStateDokter.listDokter){ dokter ->
+                        AnimatedVisibility(visible = true) {
+                            CardDokter(
+                                dokter = dokter,
+                                onClick = { onClick(dokter.id) },
+                                modifier = Modifier.animateContentSize()
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+fun Color(spesialis : String): Color {
+    return when (spesialis){
+        "Bedah Umum" -> Color.Green
+        "Jantung" -> Color.Red
+        "Penyakit Dalam" -> Color.Blue
+        "Spesialis Anak" -> Color.Yellow
+        "Gigi" -> Color.Magenta
+        else -> Color.Gray
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardDokter(
+    dokter: Dokter,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+){
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp, vertical = 6.dp)
+            .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.background),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.doctor),
+                    contentDescription = "Dokter",
+                    modifier = Modifier
+                        .size(55.dp)
+                        .clip(CircleShape)
+                        .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = dokter.nama,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Text(
+                        text = "Spesialis : ${dokter.spesialis}",
+                        fontSize = 16.sp,
+                        color = Color(dokter.spesialis),
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            HorizontalDivider(thickness = 1.dp, color = MaterialTheme.colorScheme.secondary)
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = "Lokasi",
+                    tint = MaterialTheme.colorScheme.secondary
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = " : ${dokter.klinik}",
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "Jam Kerja",
+                    tint = MaterialTheme.colorScheme.secondary
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = " : ${dokter.jamKerja}",
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+}
